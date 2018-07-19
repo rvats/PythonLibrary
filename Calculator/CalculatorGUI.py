5a98a2224f117a868ebaea96d0d1b413698a5093
Rahul Vats <vats.rahul@gmail.com>
2018-07-18 23:57:56 -0400

    Updating Python Based GUI Calculator developed using Tkinter Signed-off-by: Rahul Vats <vats.rahul@gmail.com>

--- Calculator/CalculatorGUI.py
+++ Calculator/CalculatorGUI.py
@@ -82,29 +82,40 @@ class calc:
 		self.newdiv=self.div
 
 		#Generating Buttons
-		Button(master,text="=",width=10,command=lambda:self.equals()).grid(row=4, column=4,columnspan=2)
-		Button(master,text='AC',width=3,command=lambda:self.clearall()).grid(row=1, column=4)
-		Button(master,text='C',width=3,command=lambda:self.clear1()).grid(row=1, column=5)
-		Button(master,text="+",width=3,command=lambda:self.action('+')).grid(row=4, column=3)
-		Button(master,text="x",width=3,command=lambda:self.action('x')).grid(row=2, column=3)
-		Button(master,text="-",width=3,command=lambda:self.action('-')).grid(row=3, column=3)
-		Button(master,text="÷",width=3,command=lambda:self.action(self.newdiv)).grid(row=1, column=3) 
-		Button(master,text="%",width=3,command=lambda:self.action('%')).grid(row=4, column=2)
+
+		# Row 1 Items
 		Button(master,text="7",width=3,command=lambda:self.action('7')).grid(row=1, column=0)
 		Button(master,text="8",width=3,command=lambda:self.action(8)).grid(row=1, column=1)
 		Button(master,text="9",width=3,command=lambda:self.action(9)).grid(row=1, column=2)
+		Button(master,text="÷",width=3,command=lambda:self.action(self.newdiv)).grid(row=1, column=3) 
+		Button(master,text='AC',width=3,command=lambda:self.clearall()).grid(row=1, column=4)
+		Button(master,text='C',width=3,command=lambda:self.clear1()).grid(row=1, column=5)
+		
+		# Row 2 Items
 		Button(master,text="4",width=3,command=lambda:self.action(4)).grid(row=2, column=0)
 		Button(master,text="5",width=3,command=lambda:self.action(5)).grid(row=2, column=1)
 		Button(master,text="6",width=3,command=lambda:self.action(6)).grid(row=2, column=2)
+		Button(master,text="x",width=3,command=lambda:self.action('x')).grid(row=2, column=3)
+		Button(master,text="(",width=3,command=lambda:self.action('(')).grid(row=2, column=4)
+		Button(master,text=")",width=3,command=lambda:self.action(')')).grid(row=2, column=5)
+		
+		# Row 3 Items
 		Button(master,text="1",width=3,command=lambda:self.action(1)).grid(row=3, column=0)
 		Button(master,text="2",width=3,command=lambda:self.action(2)).grid(row=3, column=1)
 		Button(master,text="3",width=3,command=lambda:self.action(3)).grid(row=3, column=2)
-		Button(master,text="0",width=3,command=lambda:self.action(0)).grid(row=4, column=0)
-		Button(master,text=".",width=3,command=lambda:self.action('.')).grid(row=4, column=1)
-		Button(master,text="(",width=3,command=lambda:self.action('(')).grid(row=2, column=4)
-		Button(master,text=")",width=3,command=lambda:self.action(')')).grid(row=2, column=5)
+		Button(master,text="-",width=3,command=lambda:self.action('-')).grid(row=3, column=3)
 		Button(master,text="√",width=3,command=lambda:self.squareroot()).grid(row=3, column=4)
 		Button(master,text="x²",width=3,command=lambda:self.square()).grid(row=3, column=5)
+		
+		# Row 4 Items
+		Button(master,text="0",width=3,command=lambda:self.action(0)).grid(row=4, column=0)
+		Button(master,text=".",width=3,command=lambda:self.action('.')).grid(row=4, column=1)
+		Button(master,text="%",width=3,command=lambda:self.action('%')).grid(row=4, column=2)
+		Button(master,text="+",width=3,command=lambda:self.action('+')).grid(row=4, column=3)
+		Button(master,text="=",width=10,command=lambda:self.equals()).grid(row=4, column=4,columnspan=2)
+		
+		# Row 5 Items		
+		
 #Main
 root = Tk()
 obj=calc(root) #object instantiated
