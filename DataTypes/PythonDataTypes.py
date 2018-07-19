0084a49bbd6476dd84acf946c9d56f83608310bd
Rahul Vats <vats.rahul@gmail.com>
2018-07-19 01:25:25 -0400

    Signed-off-by: Rahul Vats <vats.rahul@gmail.com>

---
+++ DataTypes/PythonDataTypes.py
@@ -0,0 +1,28 @@
+"""
+@Author: Rahul Vats
+@AuthorEmail: vats.rahul@gmail.com
+@Note: This is the Hello World Program
+"""
+# Declare a variable x as 5
+x = 5
+print("x = ",x,type(x))
+
+# Declare y as John
+y = "John"
+print("y = ", y,type(y))
+
+# Assign John in y to x which had 5
+x = y
+print("Assigning y to x")
+print("x = ",x,type(x))
+
+# Addign Float value 99.9 to y
+y = 99.9
+print("y = ", y,type(y))
+
+# Assign 99.9 in y to x which had John
+x = y
+print("Assigning y to x")
+print("x = ",x,type(x))
+
+print("Python is awesomer than C, C++, C#, Java, etc.")
