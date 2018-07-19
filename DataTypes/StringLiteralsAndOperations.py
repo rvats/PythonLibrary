dd805086c5c9dfdd8ee146e1bc8ec0954ad022d1
Rahul Vats <vats.rahul@gmail.com>
2018-07-19 01:47:43 -0400

    Signed-off-by: Rahul Vats <vats.rahul@gmail.com>

---
+++ DataTypes/StringLiteralsAndOperations.py
@@ -0,0 +1,11 @@
+"""
+@Author: Rahul Vats
+@AuthorEmail: vats.rahul@gmail.com
+@Note: This is the Hello World Program
+"""
+stringLiteral = "Hello"
+for i in range(5):
+    print("Character at ", i, " position in String Literal ", stringLiteral, " is ", stringLiteral[i])
+
+stringLiteral = "Universe!"
+print(stringLiteral[0:9])
