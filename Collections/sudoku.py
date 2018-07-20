0e685a4f3d7c5d1310bc2f4544f5f5a73e5d9e6b
Rahul Vats <vats.rahul@gmail.com>
2018-07-20 12:01:59 -0400

    Signed-off-by: Rahul Vats <vats.rahul@gmail.com>

--- Collections/sudoku.py
+++ Collections/sudoku.py
@@ -68,17 +68,17 @@ def eliminate(values, s, d):
     values[s] = values[s].replace(d,'')
     ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
     if len(values[s]) == 0:
-	return False ## Contradiction: removed last value
+        return False ## Contradiction: removed last value
     elif len(values[s]) == 1:
         d2 = values[s]
         if not all(eliminate(values, s2, d2) for s2 in peers[s]):
             return False
     ## (2) If a unit u is reduced to only one place for a value d, then put it there.
     for u in units[s]:
-	dplaces = [s for s in u if d in values[s]]
-	if len(dplaces) == 0:
-	    return False ## Contradiction: no place for this value
-	elif len(dplaces) == 1:
+        dplaces = [s for s in u if d in values[s]]
+        if len(dplaces) == 0:
+            return False
+        elif len(dplaces) == 1:
 	    # d can only be in one place in unit; assign it there
             if not assign(values, dplaces[0], d):
                 return False
